568296b01ac0867afc8b68be2eec3b3f3d304fc9
Adriaan van der Bergh <avanderbergh@gmail.com>
2022-11-03 17:09:46 +0100

    Update after comments

--- apps/canary/src/example.js
+++ apps/canary/src/example.js
@@ -1,26 +1,32 @@
-const TdKit = {};
-const config = {
-  realm: "talentdigital-devtd2",
-  url: "https://devtd2.talentdigit.al/auth",
-  clientId: "td-profile2",
-};
+const TalentKit = {};
+const tenantKey = "awo";
+
+// Creates a kit in development mode. Skips authentication.
+// All storage and api calls are mocked.
+const kit = await TalentKit.create();
 
 // Creates a kit, reads seasonId and episodeId from URL params
 // Reads redirectURL from URL Params
 // Authenticates the user
-const kit = await TdKit.create(config);
+kit = await TalentKit.create(tenantKey);
 
 // Creates a kit with sid: 1, eid: 1
-kit = await TdKit.create(config, { sid: 1, eid: 1 });
+kit = await TalentKit.create(tenantKey, { sid: 1, eid: 1 });
 
 // This sends a test event that passes.
-await kit.test("test1", true);
+await kit.test("test1", "pass"); // typed as "pass" | "fail";
+// or
+// Would be really good if we coukld generate "test1" as a ts type from the config files... 🤔
+await kit.test["test1"].pass(); // Maybe?
 
 // Reads the player profile name
 console.log(`Hello ${kit.profile.name}`);
 
 // Sends a test event that fails
-await kit.test("test2", false);
+await kit.test("test2", "fail"); // typed as "pass" | "fail"
+// or
+// Would be really good if we coukld generate "test2" as a ts type from the config files... 🤔
+await kit.test["test2"].fail(); // Maybe ?
 
 // Stores an arbitrary value, scoped under [sid][eid];
 await kit.save("key", "value");
