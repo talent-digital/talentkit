c6e526d55ff094e38904e1b0250c2eac24b5c243
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-01-10 18:13:53 +0100

    docs(changeset): Fix kit creation

---
+++ .changeset/spotty-garlics-grow.md
@@ -0,0 +1,5 @@
+---
+"@talentdigital/react": minor
+---
+
+Fix kit creation
--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react";
 import TalentKit, { Config } from "@talentdigital/kit";
 
-export const create =
-  <T = unknown>(config: Config) =>
-  () => {
-    let pending = false;
-    const [kit, setKit] = useState<TalentKit<T> | undefined>();
+export const create = <T = unknown>(config: Config) => {
+  const [kit, setKit] = useState<TalentKit<T> | undefined>();
+  let pending = false;
 
-    useEffect(() => {
-      if (!kit && !pending) {
-        pending = true;
-        void TalentKit.create<T>(config).then((newKit) => {
-          pending = false;
-          if (newKit) setKit(newKit);
-        });
-      }
-    }, [config]);
+  useEffect(() => {
+    if (!kit && !pending) {
+      pending = true;
+      void TalentKit.create<T>(config).then((newKit) => {
+        pending = false;
+        if (newKit) setKit(newKit);
+      });
+    }
+  }, [config]);
 
-    return kit;
-  };
+  return () => kit;
+};
