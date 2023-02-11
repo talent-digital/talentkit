bea4f43a3db7a715178821eb01b01375ab510258
krzysztof.adamczak <krzysztof.adamczak@talentdigital.eu>
2023-02-11 15:08:00 +0100

    Restore dns set

--- apps/canary/vite.config.ts
+++ apps/canary/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import dns from "dns";
 
-// dns.setDefaultResultOrder("verbatim");
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
