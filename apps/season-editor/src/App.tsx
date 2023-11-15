283b334cc8845be6d42651eb41a69604d6fd16df
Bujakowski Bartosz <bartosz.bujakowski@talentdigital.eu>
2023-11-15 08:44:50 +0100

    Add MUI to season editor

--- apps/season-editor/src/App.tsx
+++ apps/season-editor/src/App.tsx
@@ -1,5 +1,19 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import theme from "./theme";
+
 function App() {
-  return <div>Hello Editor</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>Hello Editor</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
