c3c50760f217fbe28aa9bea3aa0feedd2db95cfd
Bujakowski Bartosz <bartosz.bujakowski@talentdigital.eu>
2023-11-22 10:43:50 +0100

    Add basic information form part

--- apps/season-editor/src/components/app-container.tsx
+++ apps/season-editor/src/components/app-container.tsx
@@ -1,181 +1,152 @@
-import { Box, Divider, IconButton, TextField, styled } from "@mui/material";
+import { Alert, Box, Button, Snackbar, styled } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { useForm } from "react-hook-form";
+import { parse, stringify } from "yaml";
 
 import AddIcon from "@mui/icons-material/Add";
+import { ChangeEvent, useState } from "react";
 
-const defaultTextFieldTestValue = `
-id: frag12
-episode: 13
-level: FOUNDATION
-documentation: Ich bin in der Lage, spezifische Nachrichten im Internet zu finden.
-search:
-  links: www.tomato.com, www.pumpkin.com
-`.trim();
+type Inputs = {
+  title: string;
+  info: string;
+  assetsURL: string;
+  seasonEndMessage: string;
+};
+
+const selectedLanguage = "de";
 
 export const AppContainer = () => {
+  const { register, reset, getValues } = useForm<Inputs>();
+  const [readFileErrorMsg, setReadFileError] = useState<string | null>(null);
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files || !event.target.files[0]) {
+      return;
+    }
+
+    const file = event.target.files[0];
+    const readFile = await file.text();
+    try {
+      const parsedFiled = parse(readFile);
+      reset({
+        title: parsedFiled.title[selectedLanguage],
+        info: parsedFiled.info[selectedLanguage],
+        assetsURL: parsedFiled.assetsURL,
+        seasonEndMessage: parsedFiled.seasonEndMessage[selectedLanguage],
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        setReadFileError(error.message);
+      }
+    }
+  };
+
+  const handleExport = () => {
+    const values = mapToSeasonObject(getValues());
+    const element = document.createElement("a");
+    const file = new Blob([stringify(values)], { type: "text/plain" });
+    element.href = URL.createObjectURL(file);
+    element.download = "season.yaml";
+    element.click();
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: 4,
-        padding: 4,
-      }}
-    >
-      <SectionWrapper>
-        <h3>Basic information</h3>
-        <TextField label="title" variant="outlined" sx={{ width: "100%" }} />
-        <TextField label="info" variant="outlined" sx={{ width: "100%" }} />
-        <TextField
-          label="assetsURL"
-          variant="outlined"
+    <Box>
+      <StyledNavigation>
+        <input type="file" accept=".yml,.yaml" onChange={handleFileChange} />
+        <button onClick={handleExport}>Save and Export season.yaml</button>
+      </StyledNavigation>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: 4,
+          padding: 4,
+        }}
+      >
+        <StyledSectionWrapper>
+          <h3>Basic information</h3>
+          <StyledInput>
+            <label>Title</label>
+            <input type="text" {...register("title")} />
+          </StyledInput>
+
+          <StyledInput>
+            <label>Info</label>
+            <input type="text" {...register("info")} />
+          </StyledInput>
+
+          <StyledInput>
+            <label>assetsURL</label>
+            <input type="text" {...register("assetsURL")} />
+          </StyledInput>
+
+          <StyledInput>
+            <label>seasonEndMessage</label>
+            <input type="text" {...register("seasonEndMessage")} />
+          </StyledInput>
+        </StyledSectionWrapper>
+      </Box>
+
+      <Snackbar
+        open={readFileErrorMsg !== null}
+        message={readFileErrorMsg}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert
+          onClose={() => setReadFileError(null)}
+          severity="error"
           sx={{ width: "100%" }}
-        />
-        <TextField
-          label="seasonEndMessage"
-          variant="outlined"
-          sx={{ width: "100%" }}
-        />
-      </SectionWrapper>
-
-      <SectionWrapper>
-        <h3>Episodes</h3>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="id" variant="outlined" sx={{ width: "100%" }} />
-          <TextField label="title" variant="outlined" sx={{ width: "100%" }} />
-          <TextField
-            label="description"
-            variant="outlined"
-            sx={{ width: "100%" }}
-          />
-        </Box>
-        <div>
-          <IconButton color="success" aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </div>
-      </SectionWrapper>
-
-      <SectionWrapper>
-        <h3>Competence Areas</h3>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="id" variant="outlined" sx={{ width: "33%" }} />
-          <TextField label="name" variant="outlined" sx={{ width: "100%" }} />
-        </Box>
-        <div>
-          <IconButton color="success" aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </div>
-      </SectionWrapper>
-
-      <SectionWrapper>
-        <h3>Competences</h3>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="id" variant="outlined" sx={{ width: "100%" }} />
-          <TextField
-            label="competence area id"
-            variant="outlined"
-            sx={{ width: "100%" }}
-          />
-          <TextField label="name" variant="outlined" sx={{ width: "100%" }} />
-        </Box>
-        <div>
-          <IconButton color="success" aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </div>
-      </SectionWrapper>
-
-      <SectionWrapper>
-        <h3>Sub competences</h3>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="id" variant="outlined" sx={{ width: "100%" }} />
-          <TextField
-            label="competence id"
-            variant="outlined"
-            sx={{ width: "100%" }}
-          />
-          <TextField label="name" variant="outlined" sx={{ width: "100%" }} />
-        </Box>
-        <div>
-          <IconButton color="success" aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </div>
-      </SectionWrapper>
-
-      {/* We can omit the id and generate it but then there might be problem when we import a yaml file */}
-      <SectionWrapper>
-        <h3>Test items</h3>
-        <TextField
-          label="Test item"
-          variant="outlined"
-          sx={{ width: "100%" }}
-          defaultValue={defaultTextFieldTestValue}
-          multiline
-          rows={6}
-        />
-        <div>
-          <IconButton color="success" aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </div>
-      </SectionWrapper>
-
-      <SectionWrapper>
-        <h3>Test items (concept2)</h3>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="id" variant="outlined" sx={{ width: "100%" }} />
-          <TextField
-            label="episode"
-            variant="outlined"
-            sx={{ width: "100%" }}
-          />
-          <TextField label="level" variant="outlined" sx={{ width: "100%" }} />
-        </Box>
-        <TextField
-          label="documentation"
-          variant="outlined"
-          sx={{ width: "100%" }}
-        />
-        <TextField label="search" variant="outlined" sx={{ width: "100%" }} />
-
-        <h3>Test item</h3>
-
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="id" variant="outlined" sx={{ width: "100%" }} />
-          <TextField
-            label="episode"
-            variant="outlined"
-            sx={{ width: "100%" }}
-          />
-          <TextField label="level" variant="outlined" sx={{ width: "100%" }} />
-        </Box>
-        <TextField
-          label="documentation"
-          variant="outlined"
-          sx={{ width: "100%" }}
-        />
-        <TextField label="search" variant="outlined" sx={{ width: "100%" }} />
-
-        <div>
-          <IconButton color="success" aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </div>
-      </SectionWrapper>
+        >
+          {readFileErrorMsg}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
 
-const SectionWrapper = styled("div")(() => ({
+function mapToSeasonObject(values: Inputs) {
+  return {
+    title: {
+      [selectedLanguage]: values.title,
+    },
+    info: {
+      [selectedLanguage]: values.info,
+    },
+    assetsURL: values.assetsURL,
+    seasonEndMessage: {
+      [selectedLanguage]: values.seasonEndMessage,
+    },
+  };
+}
+
+const StyledSectionWrapper = styled("div")(({ theme }) => ({
   boxShadow: `3px 3px 10px rgba(0, 0, 0, 0.3)`,
-  borderRadius: 4,
-  padding: 16,
+  borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(2),
   width: 800,
   display: "flex",
-  gap: 16,
+  gap: theme.spacing(2),
   flexDirection: "column",
-  background: "white",
+  background: "#fff",
+}));
+
+const StyledInput = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  "& input": {
+    borderRadius: theme.shape.borderRadius,
+    border: `1px solid ${grey[500]}`,
+    padding: theme.spacing(1),
+  },
+}));
+
+const StyledNavigation = styled("div")(({ theme }) => ({
+  padding: theme.spacing(2),
+  background: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  display: "flex",
+  justifyContent: "center",
 }));
