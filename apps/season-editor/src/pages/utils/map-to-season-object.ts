e2640a11af1e01648041c7237f42f47e540a6981
bartosz-bujakowski <105653997+bartosz-bujakowski@users.noreply.github.com>
2023-12-21 10:54:28 +0100

    Change wrong competence property select when keeping old values (#126)
    * Change wrong competence property select when keeping old values
    
    This change fixes exporting old language value for competence name when
    adding a new language

--- apps/season-editor/src/pages/utils/map-to-season-object.ts
+++ apps/season-editor/src/pages/utils/map-to-season-object.ts
@@ -6,9 +6,8 @@ export function mapToSeasonObject(
   valuesWithEmpty: FormInputs,
   language: LanguageCode
 ): SeasonDefinition | ErrorObject {
-  const newFile: SeasonDefinition = JSON.parse(
-    JSON.stringify(originalFileLoaded)
-  ) as SeasonDefinition;
+  const originalFileLoadedCopy = deepCloneSeason(originalFileLoaded);
+  const newFile = deepCloneSeason(originalFileLoaded);
   const values: Partial<FormInputs> = Object.fromEntries(
     Object.entries(valuesWithEmpty).filter((entries) => Boolean(entries[1]))
   ) as Partial<FormInputs>;
@@ -37,7 +36,7 @@ export function mapToSeasonObject(
   newFile.seasonEndMessage[language] = values.seasonEndMessage;
   newFile.competenceAreas = mapToSeasonCompetenceAreas(
     values,
-    newFile,
+    originalFileLoadedCopy,
     language
   );
   newFile.episodes = mapToSeasonEpisodes(values, newFile, language);
@@ -85,7 +84,9 @@ function mapToSeasonCompetences(
         ...accumulator,
         [competence.competenceId]: {
           name: {
-            ...oldValues.competenceAreas[competence.competenceId]?.name,
+            ...oldValues.competenceAreas[competenceAreaId]?.competences[
+              competence.competenceId
+            ]?.name,
             [language]: competence.name,
           },
           subCompetences: mapToSeasonSubCompetences(
@@ -263,4 +264,8 @@ function mapToSeasonEpisodes(
       };
     }, {}) ?? {}
   );
+}
+
+function deepCloneSeason(file: SeasonDefinition): SeasonDefinition {
+  return JSON.parse(JSON.stringify(file)) as SeasonDefinition;
 }
